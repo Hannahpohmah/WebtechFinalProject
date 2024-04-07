e868940cce6153d14c3b1ea9b8f41d1b9545ad8f
Hannahpohmah <111054206+Hannahpohmah@users.noreply.github.com>
2024-04-07 22:34:44 +0000

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php      
+    $host = "localhost";  
+    $user = "root";  
+    $password = 'MBAFOR41*123#HHHH';  
+    $db_name = "home_maintenance";  
+      
+    $conn = mysqli_connect($host, $user, $password, $db_name);  
+    if(mysqli_connect_errno()) {  
+        die("Failed to connect with MySQL: ". mysqli_connect_error());  
+    }  
+?>
