c2d856bddd60ac87e3f7cfa89a04ce3b200ead7a
Hannahpohmah <111054206+Hannahpohmah@users.noreply.github.com>
2024-04-11 16:00:59 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,15 +1,15 @@
 <?php      
    $servername = "localhost";
-    $username = "bitnami";
-    $password = "n!LM6M!mHKuO";
-    $database = "home_maintenance";
+   $username = "bitnami";
+   $password = "n!LM6M!mHKuO";
+   $database = "home_maintenance";
     
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $database);
+   $conn = new mysqli($servername, $username, $password, $database);
     
     // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+   if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
     }
      
 ?>
