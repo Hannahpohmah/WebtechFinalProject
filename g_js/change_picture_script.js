7dc4e678d380a6b3e01b60c5b7b829f6f8db62c0
Hannahpohmah <111054206+Hannahpohmah@users.noreply.github.com>
2024-04-12 20:40:51 +0000

    Delete g_js directory

--- g_js/change_picture_script.js
+++
@@ -1,7 +0,0 @@
-function openModal() {
-    document.querySelector(".modal-change-picture").style.display = "block";
-}
-
-function closeModal() {
-    document.querySelector(".modal-change-picture").style.display = "none";
-}
